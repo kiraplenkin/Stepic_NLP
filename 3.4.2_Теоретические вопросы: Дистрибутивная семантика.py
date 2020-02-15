61228427437a160634453246d5fbb28bdf329a3d
kirr <kiraplenkin@gmail.com>
2020-02-15 08:52:37 +0300

    Add 3.4.2 solution

--- 3.4.2_Теоретические вопросы: Дистрибутивная семантика.py
+++ 3.4.2_Теоретические вопросы: Дистрибутивная семантика.py
@@ -11,3 +11,29 @@ def read_array():
     return parse_array(sys.stdin.readline())
 
 
+def write_array(arr):
+    print(repr(arr.tilist()))
+
+
+def generate_w2v_sgns_samples(text, window_size, vocab_size, ns_rate):
+    vocab = []
+    answer = []
+    for token in text:
+        if lem(vocab) < vocab_size:
+            if token not in vocab:
+                vocab.append(token)
+
+    max_step = window_size // 2
+
+    for i, token in enumerate(text):
+        for step in range(1, max_step+1):
+            if i - step > 0:
+                answer.append(list([text[i], text[i-step], 1]))
+                for _ in range(ns_rate):
+                    answer.append(list([text[i], np.random.choice(vocab), 0]))
+            if i + step < len(text):
+                answer.append(list([text[i], text[i+step], 1]))
+                for _ in range(ns_rate):
+                    answer.append(list([text[i], np.random.choice(vocab), 0]))
+
+    return answer
